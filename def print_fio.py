38e7132f6a66fa9f83bf1d738614a9265227b6b6
lazarkovaevgenia <ealazarkova@gmail.com>
2021-10-03 22:37:01 +0300

    Update def print_fio.py

--- def print_fio.py
+++ def print_fio.py
@@ -1,6 +1,3 @@
-Python 3.9.5 (v3.9.5:0a7dcbdb13, May  3 2021, 13:17:02) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> # объявление функции
 def print_fio(name, surname, patronymic):
     print(''.join([surname[0], name[0], patronymic[0]]).upper())
